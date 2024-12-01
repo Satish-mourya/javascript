01679c1d842b6016a243af16ea59b4ff124513ec
Satish-mourya <satishmourya.o970@gmail.com>
2024-12-01 23:42:18 +0530

    ashynchronous function

---
+++ dom/synchronous.js
@@ -0,0 +1,11 @@
+console.log("hii")
+console.log("i'm satish")
+
+// ashynchronous function
+
+console.log("greetings")
+setTimeout(()=>{
+    console.log("delay 3 sec")
+},3000)
+
+console.log("come back again")
